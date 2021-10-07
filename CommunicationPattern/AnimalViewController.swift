e4bdef7583d0f717bcea9450a2c24bb417cc4199
Albert Pangestu <stamina.left@gmail.com>
2021-10-07 12:16:22 +0700

    Created AnimalViewController for second view in Main.storyboard.

---
+++ CommunicationPattern/AnimalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AnimalViewController.swift
+//  CommunicationPattern
+//
+//  Created by Albert Pangestu on 07/10/21.
+//
+
+import UIKit
+
+class AnimalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
