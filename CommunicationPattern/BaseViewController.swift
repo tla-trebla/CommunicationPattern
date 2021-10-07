fb795f32cfacf1441f36178cd369898a8f8f4191
Albert Pangestu <stamina.left@gmail.com>
2021-10-07 12:20:51 +0700

    Added outlets (animalImageView & nameLabel) and action (animalSelectionButtonClicked).

--- CommunicationPattern/BaseViewController.swift
+++ CommunicationPattern/BaseViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class BaseViewController: UIViewController {
 
+    // MARK: - Outlets
+    
+    @IBOutlet weak var animalImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: - Actions
 
+    @IBAction func animalSelectionButtonClicked(_ sender: Any) {
+    }
 }
 
